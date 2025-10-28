af8b82cb8a9a199af0ec43e5725f9df6c8ca36fa
ArifMunandar <114490924+Arif-komp@users.noreply.github.com>
2025-10-29 00:22:27 +0800

    Update main.dart

--- main.dart
+++ main.dart
@@ -4,7 +4,7 @@ import 'package:provider/provider.dart';
 import 'inventory_provider.dart';
 import 'inventory_screen.dart';
 import 'user_provider.dart';
-import 'login_screen.dart'; // Asumsi: Anda memiliki halaman login
+import 'login_screen.dart'; 
 
 void main() {
   runApp(const MyApp());
@@ -24,63 +24,20 @@ class MyApp extends StatelessWidget {
         title: 'DEPO SUMBER BANGUNAN INVENTORY',
         theme: ThemeData(
           primarySwatch: Colors.teal,
+          appBarTheme: const AppBarTheme(
+            backgroundColor: Colors.teal,
+          ),
           visualDensity: VisualDensity.adaptivePlatformDensity,
-          fontFamily: 'Poppins', // Pastikan font Poppins tersedia atau hapus baris ini
+          // font family Poppins
         ),
-        // Menggunakan Consumer untuk menentukan halaman mana yang harus ditampilkan (Login/Inventory)
         home: Consumer<UserProvider>(
           builder: (context, userProvider, child) {
-            // Jika user aktif ada, tampilkan InventoryScreen
             if (userProvider.activeUser != null) {
               return const InventoryScreen();
             } else {
-              // Jika tidak ada user, arahkan ke LoginScreen
-              return const LoginScreen(); // Ganti dengan widget login Anda
+              return const LoginScreen();
             }
           },
-        ),
-      ),
-    );
-  }
-}
-
-
-// Contoh sederhana Login Screen. Anda harus membuat file login_screen.dart
-class LoginScreen extends StatelessWidget {
-  const LoginScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    final usernameController = TextEditingController();
-    final userProvider = Provider.of<UserProvider>(context, listen: false);
-
-    return Scaffold(
-      appBar: AppBar(title: const Text('Login User')),
-      body: Padding(
-        padding: const EdgeInsets.all(30.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text('Masukkan Nama User', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-            const SizedBox(height: 20),
-            TextField(
-              controller: usernameController,
-              textCapitalization: TextCapitalization.characters,
-              decoration: const InputDecoration(
-                labelText: 'Nama User (Contoh: BOBI)',
-                border: OutlineInputBorder(),
-              ),
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                if (usernameController.text.trim().isNotEmpty) {
-                  userProvider.login(usernameController.text);
-                }
-              },
-              child: const Text('MASUK'),
-            ),
-          ],
         ),
       ),
     );
