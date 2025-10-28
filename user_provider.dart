b66a53191e1006115c5bb3e662d950d68f11357a
ArifMunandar <114490924+Arif-komp@users.noreply.github.com>
2025-10-29 00:19:29 +0800

    Update user_provider.dart

--- user_provider.dart
+++ user_provider.dart
@@ -17,7 +17,6 @@ class UserProvider extends ChangeNotifier {
     notifyListeners();
   }
 
-  // Menggantikan logika login di index.html/script.js
   Future<bool> login(String username) async {
     if (username.trim().isEmpty) return false;
     
@@ -28,7 +27,6 @@ class UserProvider extends ChangeNotifier {
     return true;
   }
 
-  // Menggantikan fungsi JS: handleLogout()
   Future<void> logout() async {
     final prefs = await SharedPreferences.getInstance();
     await prefs.remove('activeUser');
